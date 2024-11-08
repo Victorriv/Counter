66de5ad0c129bf70858f198ac9e46ce04b218d71
Victorriv <Victorrivera714@yahoo.com>
2024-11-08 07:06:18 -0800

    const for count and let

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const decrease = document.ElementById("decreaseBtn");
 const resetBtn = document.ElementById("resetBtn");
 const increaseBtn = document.ElementById("increaseBtn");
+const countLabel = document.ElementById("countLabel");
+let count = 0;
