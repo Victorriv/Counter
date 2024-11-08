1f97919b0b1222c52534eb906700c0512f9b8d46
Victorriv <Victorrivera714@yahoo.com>
2024-11-08 07:11:25 -0800

    forgot to add the word get

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const decrease = document.ElementById("decreaseBtn");
-const resetBtn = document.ElementById("resetBtn");
-const increaseBtn = document.ElementById("increaseBtn");
-const countLabel = document.ElementById("countLabel");
-let count = 0;
+const decrease = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0; // will be reassigning count, incrementing and decrementing it. 
+
+// now I need 3 functions... 
+// increase, decrease and reset. 
+
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+    
+}
